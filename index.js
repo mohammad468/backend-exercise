254fc4cf06e1880b79a91872c9713cccb6a95930
mohammad mohseni <mohammad.mohseni468@gmail.com>
2022-03-09 18:18:18 +0330

    switch case

---
+++ index.js
@@ -0,0 +1,28 @@
+let day = 0;
+switch (day) {
+  case 0:
+    console.log("shanbe");
+    break;
+  case 1:
+    console.log("1 shanbe");
+    break;
+  case 2:
+    console.log("2 shanbe");
+    break;
+  case 3:
+    console.log("3 shanbe");
+    break;
+  case 4:
+    console.log("4 shanbe");
+    break;
+  case 5:
+    console.log("5 shanbe");
+    break;
+  case 6:
+    console.log("jomee");
+    break;
+
+  default:
+    console.log("this day its not found");
+    break;
+}
