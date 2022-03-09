e62048440f2f6ff9febccda09e61bd1281401bc6
mohammad mohseni <mohammad.mohseni468@gmail.com>
2022-03-09 18:28:34 +0330

    for

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
-let counter = 0;
-while (counter <= 10) {
-  console.log(counter);
-  counter++;
+for (let i = 0; i <= 6; i++) {
+  switch (i) {
+    case 0:
+      console.log("shanbe");
+      break;
+    case 1:
+      console.log("1 shanbe");
+      break;
+    case 2:
+      console.log("2 shanbe");
+      break;
+    case 3:
+      console.log("3 shanbe");
+      break;
+    case 4:
+      console.log("4 shanbe");
+      break;
+    case 5:
+      console.log("5 shanbe");
+      break;
+    case 6:
+      console.log("jomee");
+      break;
+    default:
+      console.log("not found");
+      break;
+  }
 }
