fc99d36b1814b396c2fb2f1f4e94f9439b81e07f
mohammad mohseni <mohammad.mohseni468@gmail.com>
2022-03-09 18:22:34 +0330

    while

--- index.js
+++ index.js
@@ -1,28 +1,5 @@
-let day = 0;
-switch (day) {
-  case 0:
-    console.log("shanbe");
-    break;
-  case 1:
-    console.log("1 shanbe");
-    break;
-  case 2:
-    console.log("2 shanbe");
-    break;
-  case 3:
-    console.log("3 shanbe");
-    break;
-  case 4:
-    console.log("4 shanbe");
-    break;
-  case 5:
-    console.log("5 shanbe");
-    break;
-  case 6:
-    console.log("jomee");
-    break;
-
-  default:
-    console.log("this day its not found");
-    break;
+let counter = 0;
+while (counter <= 10) {
+  console.log(counter);
+  counter++;
 }
