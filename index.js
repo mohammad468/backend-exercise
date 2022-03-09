b2457d5c10c6026877faeca2bcb2839b6ae0b3c5
mohammad mohseni <mohammad.mohseni468@gmail.com>
2022-03-10 01:06:49 +0330

    remove item as avale, akhar array

--- index.js
+++ index.js
@@ -8,8 +8,16 @@ console.log("new array = ", numbers);
 
 // add item to akhare array
 numbers.push(41);
-console.log("new array = ", numbers);
+console.log("new array with push = ", numbers);
 
 // add item to avale array
 numbers.unshift(14);
-console.log("new array = ", numbers);
+console.log("new array with unshift = ", numbers);
+
+// remove item as akhar array
+numbers.pop(25);
+console.log("new array with pop = ", numbers);
+
+// remove item as avale array
+numbers.shift(4);
+console.log("new array with shift = ", numbers);
