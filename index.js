b0d86592422cdea91cca7d2f7f3814dab7bd2e55
mohammad mohseni <mohammad.mohseni468@gmail.com>
2022-03-09 18:44:02 +0330

    return in fanction

--- index.js
+++ index.js
@@ -1,28 +1,9 @@
-for (let i = 0; i <= 6; i++) {
-  switch (i) {
-    case 0:
-      console.log("shanbe");
-      break;
-    case 1:
-      console.log("1 shanbe");
-      break;
-    case 2:
-      console.log("2 shanbe");
-      break;
-    case 3:
-      console.log("3 shanbe");
-      break;
-    case 4:
-      console.log("4 shanbe");
-      break;
-    case 5:
-      console.log("5 shanbe");
-      break;
-    case 6:
-      console.log("jomee");
-      break;
-    default:
-      console.log("not found");
-      break;
-  }
+let num1 = 5,
+  num2 = 10;
+
+function Calculator() {
+  return num1 + num2;
 }
+
+const result = Calculator();
+console.log(result);
