06930dc8e9911124928bc39624e6cc5d707f6229
mohammad mohseni <mohammad.mohseni468@gmail.com>
2022-03-10 14:34:22 +0330

    convert array string to number with map

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-console.log(numbers);
-let newNumbers = numbers.map((number) => number * 2);
-console.log(newNumbers);
+let strNumbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const convertedNumbers = strNumbers.map(Number);
+console.log(convertedNumbers);
