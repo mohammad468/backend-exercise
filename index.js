364a3d9b78b3cdfe69253649d977d59bb5c04895
mohammad mohseni <mohammad.mohseni468@gmail.com>
2022-03-10 14:45:43 +0330

    گرفتن تاریخ شمسی

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-let strNumbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
-const convertedNumbers = strNumbers.map(Number);
-console.log(convertedNumbers);
+// * Date and Time
+const date = new Date();
+console.log(date.toLocaleString("fa-IR"));
