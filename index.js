c4250f508553cf73b7721482979c6a1fd76c44d9
mohammad mohseni <mohammad.mohseni468@gmail.com>
2022-03-10 14:27:44 +0330

    loop and push in new array

--- index.js
+++ index.js
@@ -1,23 +1,5 @@
-let numbers = [1, 4, 8, 7, 0, 20, 5, 6];
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 console.log(numbers);
-console.log("array length = ", numbers.length - 1);
-console.log("typeof = ", typeof numbers);
-numbers[numbers.length] = 55;
-numbers[numbers.length] = 25;
-console.log("new array = ", numbers);
-
-// add item to akhare array
-numbers.push(41);
-console.log("new array with push = ", numbers);
-
-// add item to avale array
-numbers.unshift(14);
-console.log("new array with unshift = ", numbers);
-
-// remove item as akhar array
-numbers.pop(25);
-console.log("new array with pop = ", numbers);
-
-// remove item as avale array
-numbers.shift(4);
-console.log("new array with shift = ", numbers);
+let newNumbers = [];
+numbers.forEach((item) => newNumbers.push(item * 2));
+console.log(newNumbers);
