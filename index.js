892ea2894e2c5a183c7bd4fe8a3671c0413eb1dd
mohammad mohseni <mohammad.mohseni468@gmail.com>
2022-03-10 14:30:19 +0330

    map in array

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 console.log(numbers);
-let newNumbers = [];
-numbers.forEach((item) => newNumbers.push(item * 2));
+let newNumbers = numbers.map((number) => number * 2);
 console.log(newNumbers);
