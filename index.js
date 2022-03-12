d4ad3e72a4f7e936c166c900a01db455c82783d6
mohammad mohseni <mohammad.mohseni468@gmail.com>
2022-03-12 16:42:55 +0330

    restFull api

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
+const { profile, products, basket, comments } = require("./Api.js");
 const http = require("http");
 http
   .createServer((req, res) => {
-    if (req.url == "/profile") {
-      const profile = {
-        name: "mohammad",
-        family: "mohseni",
-        age: 22,
-        wallet: "300000$",
-      };
-      res.write(JSON.stringify(profile));
+    switch (req.url) {
+      case "/api/profile":
+        res.write(JSON.stringify(profile));
+        break;
+      case "/api/products":
+        res.write(JSON.stringify(products));
+        break;
+      case "/api/basket":
+        res.write(JSON.stringify(basket));
+        break;
+      case "/api/comments":
+        res.write(JSON.stringify(comments));
+        break;
+      default:
+        res.write(
+          JSON.stringify({
+            status: 404,
+            message: "Not Found",
+          })
+        );
+        break;
     }
     res.end();
   })
