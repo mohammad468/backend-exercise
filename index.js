8b7018b89ff9da38c3fa67f7da217db335f91707
mohammad mohseni <mohammad.mohseni468@gmail.com>
2022-03-12 16:10:29 +0330

    create object & convert to string & send to client

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const http = require("http");
 http
   .createServer((req, res) => {
-    res.write("hello http server on nodejs");
+    if (req.url == "/profile") {
+      const profile = {
+        name: "mohammad",
+        family: "mohseni",
+        age: 22,
+        wallet: "300000$",
+      };
+      res.write(JSON.stringify(profile));
+    }
     res.end();
   })
   .listen(3000, () => {
