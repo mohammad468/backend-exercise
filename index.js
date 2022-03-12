dfec4f0d3d3b789b235f086b4335f0dc5dcc4d38
mohammad mohseni <mohammad.mohseni468@gmail.com>
2022-03-12 17:44:28 +0330

    upload file

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const { profile, products, basket, comments } = require("./Api.js");
+const fs = require("fs");
+const path = require("path");
+const formidable = require("formidable");
 const http = require("http");
 http
   .createServer((req, res) => {
@@ -14,6 +17,24 @@ http
         break;
       case "/api/comments":
         res.write(JSON.stringify(comments));
+        break;
+      case "/fileupload":
+        if (req.method.toLowerCase() === "post") {
+          if (!fs.existsSync(path.join(__dirname, "uploads", "files"))) {
+            fs.mkdirSync("uploads/files", { recursive: true });
+          }
+          const form = new formidable.IncomingForm({
+            uploadDir: path.join(__dirname, "uploads", "files"),
+            keepExtensions: true,
+            multiples: true,
+            maxFileSize: 5 * 1024 * 1024,
+            allowEmptyFiles: false,
+          });
+          form.parse(req, function (err, fields, files) {});
+        } else {
+          const htmlForm = fs.readFileSync("./uploadfile.html", "utf-8");
+          res.write(htmlForm);
+        }
         break;
       default:
         res.write(
