089a0741b73e01fb8ea637ebaad29fc421c8f50d
mohammad mohseni <mohammad.mohseni468@gmail.com>
2022-04-15 00:59:13 +0430

    hellow world in client

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 
+app.get("/", (req, res, next) => {
+  res.send("Hello world");
+});
 app.listen(3000, () => {
   console.log("server run port 3000");
 });
