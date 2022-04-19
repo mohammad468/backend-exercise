9cc6d78a8f60491f6910d524122e098edc34a7c3
mohammad mohseni <mohammad.mohseni468@gmail.com>
2022-04-19 16:07:00 +0430

    404

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+//my middleware
+app.use((req, res, next) => {
+  req.request_date = new Date();
+  next();
+});
+
 app.get("/", (req, res) => {
   res.send("Hello world");
 });
@@ -25,6 +31,14 @@ app.post("/post-data/:name", (req, res) => {
     body: req.body,
     query: req.query,
     params: req.params,
+    requestDate: req.request_date,
+  });
+});
+app.use((req, res, next) => {
+  return res.status(404).json({
+    status: 404,
+    success: false,
+    message: "this route not found",
   });
 });
 app.listen(3000, () => {
